746a85dcfae13ab5371befff946de7a6b6495fed
faruqalawy <faruqhudaya@gmail.com>
2024-10-20 09:00:24 +0700

    change api url 2

--- src/services/locationService.js
+++ src/services/locationService.js
@@ -2,23 +2,23 @@ import axios from "axios";
 
 // Getting array data for list of locations
 export const getProvincies = async () => {
-  return axios.get("https://emsifa.github.io/api-wilayah-indonesia/api/provinces.json");
+  return axios.get("/api/api-wilayah-indonesia/api/provinces.json");
 };
 
 export const getRegencies = async (provinceId) => {
   return axios.get(
-    `https://emsifa.github.io/api-wilayah-indonesia/api/regencies/${provinceId}.json`
+    `/api/api-wilayah-indonesia/api/regencies/${provinceId}.json`
   );
 };
 
 export const getDistricts = async (regencyId) => {
   return axios.get(
-    `https://emsifa.github.io/api-wilayah-indonesia/api/districts/${regencyId}.json`
+    `/api/api-wilayah-indonesia/api/districts/${regencyId}.json`
   );
 };
 
 export const getVillages = async (districtId) => {
   return axios.get(
-    `https://emsifa.github.io/api-wilayah-indonesia/api/villages/${districtId}.json`
+    `/api/api-wilayah-indonesia/api/villages/${districtId}.json`
   );
 };

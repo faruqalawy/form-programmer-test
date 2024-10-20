b5ab4930146d88096d9770452b08f0d3e8d9d5d0
faruqalawy <faruqhudaya@gmail.com>
2024-10-20 08:51:43 +0700

    delete base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/form-programmer-test/",
   plugins: [vue()],
   server: {
     proxy: {

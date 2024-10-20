e8f795f12a61b67351784ab0fe74693717d09600
faruqalawy <faruqhudaya@gmail.com>
2024-10-20 08:33:34 +0700

    second commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/form-programmer-test/",
   plugins: [vue()],
   server: {
     proxy: {

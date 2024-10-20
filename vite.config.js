ad19b82dabbe408c1d63575d13fa217a3a68233e
faruqalawy <faruqhudaya@gmail.com>
2024-10-20 09:09:00 +0700

    remove base url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/form-programmer-test.vercel.app/",
+  // base: "/form-programmer-test.vercel.app/",
   plugins: [vue()],
   server: {
     proxy: {
